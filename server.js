45b8ddcf63d0f7ceedb4b645257b08d5d875eb53
cvasit <cvasit@gmail.com>
2017-05-08 12:16:08 -0500

    Prep repo for heroku

--- server.js
+++ server.js
@@ -3,12 +3,21 @@ var express =require('express');
 //create your app
 var app=express();
 
+app.use(function (req,res,next) {
+    if(req.headers['x-forwarded-proto']=='http')
+        next();
+    else
+        response.redirect('http://' + req.hostname + req.url)
+});
+
 //tell express which folder to server exposing the folder to webserver
 app.use(express.static('public'));
 
 var portnumber=3000;
 
 app.listen(portnumber,function () {
-    var port=portnumber;
+
+    var port=process.env.PORT || portnumber;
+
     console.log(`Express server is up on port:${port}`)
 });
